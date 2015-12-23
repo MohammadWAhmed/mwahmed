487c6b7edfa7fea91dcde552ad04924f09962275
Mohammad W. Ahmed <wasiuddin.ahmed@hotmail.com>
2015-12-23 17:37:40 -0500

    trying to fix prod assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,97 +1,46 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # Code is not reloaded between requests.
+  # In the development environment your application's code is reloaded on
+  # every request. This slows down response time but is perfect for development
+  # since you don't have to restart the web server when you make code changes.
   config.cache_classes = true
 
-  # Eager load code on boot. This eager loads most of Rails and
-  # your application in memory, allowing both thread web servers
-  # and those relying on copy on write to perform better.
-  # Rake tasks automatically ignore this option for performance.
+  # Do not eager load code on boot.
   config.eager_load = true
 
-  # Full error reports are disabled and caching is turned on.
+  # Show full error reports and disable caching.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Enable Rack::Cache to put a simple HTTP cache in front of your application
-  # Add `rack-cache` to your Gemfile before enabling this.
-  # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
-  # config.action_dispatch.rack_cache = true
-
-  # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true
-
-  # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
-  config.assets.css_compressor = :sass
-
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
-
-  # Generate digests for assets URLs.
-  config.assets.digest = true
-
-  # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.0'
-
-  # Specifies the header that your server uses for sending files.
-  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
-
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = false
-
-  # Set to :debug to see everything in the log.
+  # Logging
   config.log_level = :info
+  config.logger = ActiveSupport::Logger.new(STDOUT)
+  config.colorize_logging = true
 
-  # Prepend all log lines with the following tags.
-  # config.log_tags = [ :subdomain, :uuid ]
-
-  # Use a different logger for distributed setups.
-  # Use STDOUT for Heroku: https://devcenter.heroku.com/articles/logging#writing-to-your-log
-  config.logger = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
-  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
-
-  #
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = "http://assets.example.com"
-
-  # Precompile additional assets.
-  # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( head.js ie.js emails/inline.css emails/include.css )
-
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
-
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
-  config.action_mailer.smtp_settings = {
-    user_name:      ENV['SENDGRID_USERNAME'],
-    password:       ENV['SENDGRID_PASSWORD'],
-    domain:         ENV['MAIL_HOST'],
-    address:       'smtp.sendgrid.net',
-    port:          '587',
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
 
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation can not be found).
-  config.i18n.fallbacks = true
+  # Location of mailer previews
+  config.action_mailer.preview_path = "#{Rails.root}/test/mailers/previews"
 
-  # Send deprecation notices to registered listeners.
-  config.active_support.deprecation = :notify
+  # Print deprecation notices to the Rails logger.
+  config.active_support.deprecation = :log
 
-  # Disable automatic flushing of the log to improve performance.
-  # config.autoflush_log = false
+  # Raise an error on page load if there are pending migrations
+  config.active_record.migration_error = :page_load
 
-  # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
+  # Debug mode disables concatenation and preprocessing of assets.
+  # This option may cause significant delays in view rendering with a large
+  # number of complex assets.
+  config.assets.debug = true
 
-  # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
+  # Adds additional error checking when serving assets at runtime.
+  # Checks for improperly declared sprockets dependencies.
+  # Raises helpful error messages.
+  config.assets.raise_runtime_errors = false
 
-  config.assets.compress = true
+  # Raises error for missing translations
+  # config.action_view.raise_on_missing_translations = true
 end
