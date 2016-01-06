bbfb69097609202d4e84837a77cc431b7b1988a1
Mohammad W. Ahmed <wasiuddin.ahmed@hotmail.com>
2016-01-05 22:38:37 -0500

    temporarily disabling bitly call

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -12,7 +12,8 @@ class ProjectsController < ApplicationController
 	def albums
 		@url_text = params["urls"]
 		@urls = !@url_text.nil? ? @url_text.split(",") : []
-		@bitly = Bitly.client.shorten(@x.to_s) if Rails.env.production?
+		#@bitly = Bitly.client.shorten(@x.to_s) if Rails.env.production?
+		@bitly = nil
 	end
   	
 end
